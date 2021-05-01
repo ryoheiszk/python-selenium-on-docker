c8517f378a49f2df617bc592ddea924ad422b192
ryoheiszk <63033551+ryoheiszk@users.noreply.github.com>
2021-05-01 10:46:46 +0900

    debug

--- app/source/test.py
+++ app/source/test.py
@@ -6,12 +6,6 @@ from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-gpu")
-options.add_argument("--window-size=1280,1024")
-
 driver = webdriver.Remote(
     command_executor=os.environ["SELENIUM_URL"],
     desired_capabilities=DesiredCapabilities.FIREFOX.copy()
