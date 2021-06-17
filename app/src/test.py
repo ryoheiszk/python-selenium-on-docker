2a67b29a03ab47bde10dccfcccc5292c2340cdba
ryoheiszk <63033551+ryoheiszk@users.noreply.github.com>
2021-06-17 09:21:51 +0900

    update

--- app/src/test.py
+++ app/src/test.py
@@ -1,6 +1,4 @@
-from datetime import datetime as dt
 import os
-from time import sleep
 
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
@@ -10,11 +8,9 @@ driver = webdriver.Remote(
     command_executor=os.environ["SELENIUM_URL"],
     desired_capabilities=DesiredCapabilities.FIREFOX.copy()
 )
-driver.get("https://www.nict.go.jp/JST/JST5.html")
 
-driver.implicitly_wait(3)
-sleep(1)
+driver.get("https://www.time-j.net/worldtime/country/jp")
+driver.implicitly_wait(5)
 
-driver.save_screenshot("{0:%Y_%m_%d__%H_%M_%S}".format(dt.now()) + ".png")
-
+print(driver.find_element_by_xpath("/html/body/div/div[6]/div[1]/div/p[5]").text)
 driver.quit()
