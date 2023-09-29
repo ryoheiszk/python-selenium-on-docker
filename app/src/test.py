dd82daac5516f51b084f2f9ec254f6cf418bf4ad
ryoheiszk <63033551+ryoheiszk@users.noreply.github.com>
2023-09-29 12:49:39 +0900

    Update test.py

--- app/src/test.py
+++ app/src/test.py
@@ -8,9 +8,10 @@ driver = webdriver.Remote(
     command_executor=os.environ["SELENIUM_URL"],
     desired_capabilities=DesiredCapabilities.FIREFOX.copy()
 )
-
-driver.get("https://www.time-j.net/worldtime/country/jp")
 driver.implicitly_wait(5)
 
+# ここからコーディング
+
+driver.get("https://www.time-j.net/worldtime/country/jp")
 print(driver.find_element_by_xpath("/html/body/div/div[6]/div[1]/div/p[5]").text)
 driver.quit()
